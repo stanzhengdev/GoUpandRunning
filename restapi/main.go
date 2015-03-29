71e2311e5d69a0f7f1a05dfe6dae30b38160580e
Stanley Zheng <dev.stanzheng@gmail.com>
2015-03-28 23:45:13 -0400

    fake api

--- restapi/main.go
+++ restapi/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+type book struct {
+	title string
+	isbn  string
+}
+
+var Shelf = make(map[book]string)
+
 func main() {
+
 	http.HandleFunc("/book", bookHandler)
 
 	err := http.ListenAndServe(":9000", nil)
@@ -17,8 +27,19 @@ func main() {
 }
 
 func bookHandler(w http.ResponseWriter, r *http.Request) {
+
+	body, _ := ioutil.ReadAll(r.Body)
+	var f interface{}
+	data := json.Unmarshal(body, &f)
+	m := f.(map[string]interface{})
+
+	if data != nil {
+		fmt.Println(m)
+	}
+
 	if r.Method == "GET" {
-		fmt.Fprintf(w, r.Method)
+		fmt.Fprintf(w, "foo")
+
 	} else if r.Method == "POST" {
 		fmt.Fprintf(w, "%s", r.Method)
 	} else if r.Method == "PUT" {
